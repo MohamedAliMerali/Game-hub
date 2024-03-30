4845950db3fa29115fc4ac94a482fab36f854875
Mohamed Ali Merali <77352913+MuhammedMiiRanna@users.noreply.github.com>
2024-03-30 23:00:57 +0100

    adding styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,22 @@ export default {
       colors: {
         "bg-light": "#fefefe",
         "bg-dark": "#191919",
+
         "primary-light": "#151515",
         "primary-dark": "#ececec",
+
         "secondary-light": "#dfdfdf",
         "secondary-dark": "#303030",
         "icon-light": "#6f6f6f",
         "icon-dark": "#a2a2a2",
 
         "btn-bg": "#22c55e",
-        "btn-color-light": "#151515",
-        "btn-color-dark": "#1f7d41",
+
+        "rating-bg-dark": "#434f47",
+        "rating-color-dark": "#9ee8bf",
+
+        "rating-bg-light": "#bdf0cc",
+        "rating-color-light": "#103c21",
       },
     },
   },
