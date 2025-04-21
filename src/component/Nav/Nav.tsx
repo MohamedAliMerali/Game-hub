397563b7650a37cc3ead9bc9a83afefb03a7828c
Mohamed Ali Merali <77352913+MohamedAliMerali@users.noreply.github.com>
2025-04-22 00:22:57 +0100

    nav bar search updated
    when u use nav bar to search when you're in a game page, it will take you to the main page and search for a game

--- src/component/Nav/Nav.tsx
+++ src/component/Nav/Nav.tsx
@@ -1,31 +1,32 @@
 import { FieldValues, useForm } from "react-hook-form";
-import { FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 import useGameQueryStore from "../../stores/gameQueryStore";
 import DarkModeToggle from "../DarkMode/DarkModeToggle";
 import LogoLink from "./LogoLink";
+import { FaSearch } from "react-icons/fa";
 
 const Nav = () => {
   const { register, handleSubmit } = useForm();
-  const { setSearchText } = useGameQueryStore();
+  const setSearchText = useGameQueryStore((s) => s.setSearchText);
+  const navigate = useNavigate();
 
   const onSubmit = (data: FieldValues) => {
     // event.preventDefault();
     console.log("data", data);
     setSearchText(data.search);
+    navigate("/");
   };
 
   return (
     <nav className="bg-transparent flex flex-row items-center py-6 px-3">
+      {/* Logo */}
       <LogoLink />
 
       {/* ssearch Bar */}
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="bg-secondary-light dark:bg-secondary-dark rounded-full py-4 px-6 space-x-4 mr-6 grow flex flex-row items-center"
+        className="bg-secondary-light dark:bg-secondary-dark rounded-full py-4 px-6 mr-6 grow flex flex-row-reverse items-center"
       >
-        <button type="submit" className="outline-none border-none">
-          <FaSearch className="text-icon-light dark:text-bg-light" />
-        </button>
         <input
           {...register("search", {
             required: true,
@@ -36,6 +37,9 @@ const Nav = () => {
           placeholder="Search games..."
           className="bg-transparent grow outline-none"
         />
+        <button type="submit" className="outline-none border-none mr-4">
+          <FaSearch className="text-icon-light dark:text-bg-light" />
+        </button>
       </form>
 
       {/* Dark mode toggle */}
